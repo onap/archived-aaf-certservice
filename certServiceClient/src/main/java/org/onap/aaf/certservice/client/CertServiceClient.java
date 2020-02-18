78f01478987bf0693f8de87649c6104637939951
kjaniak <kornel.janiak@nokia.com>
2020-02-18 13:25:43 +0100

    Add first test of System.exit
    Issue-ID: AAF-996
    Signed-off-by: kjaniak <kornel.janiak@nokia.com>
    Change-Id: Id0ee4d6a08e7050ef080f0e1fadea8c07459571c

---
+++ certServiceClient/src/main/java/org/onap/aaf/certservice/client/CertServiceClient.java
@@ -0,0 +1,11 @@
+package org.onap.aaf.certservice.client;
+
+public class CertServiceClient {
+    public void run(String[] args) {
+        exit(0);
+    }
+
+    protected void exit(int statusCode){
+        System.exit(statusCode);
+    }
+}
