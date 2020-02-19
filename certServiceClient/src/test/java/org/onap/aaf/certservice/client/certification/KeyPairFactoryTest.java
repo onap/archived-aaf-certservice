8cb06ac02c04d27af0e6228b9e8f6adf9ec530fa
kjaniak <kornel.janiak@nokia.com>
2020-02-19 16:42:38 +0100

    Implementation of KeyPair generation
    Issue-ID: AAF-996
    Change-Id: I97626d122033127d5bb234d134c20d0b5a6d7c03
    Signed-off-by: kjaniak <kornel.janiak@nokia.com>

---
+++ certServiceClient/src/test/java/org/onap/aaf/certservice/client/certification/KeyPairFactoryTest.java
@@ -0,0 +1,39 @@
+/*============LICENSE_START=======================================================
+ * aaf-certservice-client
+ * ================================================================================
+ * Copyright (C) 2020 Nokia. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.aaf.certservice.client.certification;
+
+
+import org.junit.jupiter.api.Test;
+
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+
+class KeyPairFactoryTest {
+
+    @Test
+    public void shouldProvideKeyPairGenerator_whenCreateKeyPairGeneratorCalled() throws NoSuchAlgorithmException {
+        //  given
+        KeyPairGenerator keyPairGenerator;
+        KeyPairFactory keyPairFactory = new KeyPairFactory(EncryptionAlgorithmConstants.RSA_ENCRYPTION_ALGORITHM,
+                EncryptionAlgorithmConstants.KEY_SIZE);
+        //  when
+        keyPairGenerator = keyPairFactory.createKeyPairGenerator();
+    }
+}
