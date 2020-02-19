5e6037f697dbd31a62f73bf9ea1390af670fc981
kjaniak <kornel.janiak@nokia.com>
2020-02-19 16:42:38 +0100

    Implementation of KeyPair generation
    Issue-ID: AAF-996
    Change-Id: I97626d122033127d5bb234d134c20d0b5a6d7c03
    Signed-off-by: kjaniak <kornel.janiak@nokia.com>

---
+++ certServiceClient/src/main/java/org/onap/aaf/certservice/client/certification/EncryptionAlgorithmConstants.java
@@ -0,0 +1,6 @@
+package org.onap.aaf.certservice.client.certification;
+
+public class EncryptionAlgorithmConstants {
+    public static final String RSA_ENCRYPTION_ALGORITHM = "RSA";
+    public static final int KEY_SIZE = 2048;
+}
