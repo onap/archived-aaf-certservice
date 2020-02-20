eaedfabb02b67089f25682ea89852c84b806f403
Tomasz Wrobel <tomasz.wrobel@nokia.com>
2020-02-20 16:45:12 +0100

    Add HttpClient
    Issue-ID: AAF-996
    Signed-off-by: Tomasz Wrobel <tomasz.wrobel@nokia.com>
    Change-Id: I3ebb0ea88ef1a72c16064fd9a1931943b57410d8

---
+++ certServiceClient/src/test/java/org/onap/aaf/certservice/client/TestData.java
@@ -0,0 +1,13 @@
+package org.onap.aaf.certservice.client;
+
+public final class TestData {
+
+    private TestData(){
+
+    }
+
+    public static final String TEST_CSR = "";
+    public static final String TEST_PK = "";
+
+
+}
