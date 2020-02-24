7dbf8e7ec9f2e208eb6963d5a5ec2a7ae5854ac8
EmmettCox <emmett.cox@est.tech>
2020-02-24 13:55:34 +0000

    Verifying certchain and returning certchain and TrustStore
    Issue-ID: AAF-1037
    Signed-off-by: EmmettCox <emmett.cox@est.tech>
    Change-Id: Iaab754ff5f568b2f2e1aeac8dbed279e20b09b3b

--- certService/src/main/java/org/onap/aaf/certservice/cmpv2client/impl/CmpMessageHelper.java
+++ certService/src/main/java/org/onap/aaf/certservice/cmpv2client/impl/CmpMessageHelper.java
@@ -1,6 +1,7 @@
-/*
- * Copyright (C) 2020 Ericsson Software Technology AB. All rights reserved.
- *
+/*-
+ * ============LICENSE_START=======================================================
+ *  Copyright (C) 2020 Nordix Foundation.
+ * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -11,15 +12,20 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * ============LICENSE_END=========================================================
  */
 
 package org.onap.aaf.certservice.cmpv2client.impl;
 
 import static org.onap.aaf.certservice.cmpv2client.impl.CmpUtil.generateProtectedBytes;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.KeyPair;
 import java.security.MessageDigest;
@@ -27,10 +33,23 @@ import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
 import java.security.Signature;
 import java.security.SignatureException;
+import java.security.cert.CertPath;
+import java.security.cert.CertPathValidator;
+import java.security.cert.CertPathValidatorException;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.CertificateParsingException;
+import java.security.cert.PKIXCertPathChecker;
+import java.security.cert.PKIXCertPathValidatorResult;
+import java.security.cert.PKIXParameters;
+import java.security.cert.TrustAnchor;
+import java.security.cert.X509Certificate;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
-import java.util.Optional;
+import java.util.Objects;
 import javax.crypto.Mac;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
@@ -42,6 +61,9 @@ import org.bouncycastle.asn1.DEROctetString;
 import org.bouncycastle.asn1.DEROutputStream;
 import org.bouncycastle.asn1.DERSequence;
 import org.bouncycastle.asn1.DERTaggedObject;
+import org.bouncycastle.asn1.cmp.CMPCertificate;
+import org.bouncycastle.asn1.cmp.CertRepMessage;
+import org.bouncycastle.asn1.cmp.ErrorMsgContent;
 import org.bouncycastle.asn1.cmp.PBMParameter;
 import org.bouncycastle.asn1.cmp.PKIBody;
 import org.bouncycastle.asn1.cmp.PKIHeader;
@@ -61,6 +83,7 @@ import org.bouncycastle.asn1.x509.KeyUsage;
 import org.bouncycastle.asn1.x509.Time;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.onap.aaf.certservice.cmpv2client.exceptions.CmpClientException;
+import org.onap.aaf.certservice.cmpv2client.exceptions.PkiErrorException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -120,8 +143,8 @@ public final class CmpMessageHelper {
     } catch (IOException ioe) {
       CmpClientException cmpClientException =
           new CmpClientException(
-              "Exception occurred while creating proof of possession for PKIMessage", ioe);
-      LOG.error("Exception occurred while creating proof of possession for PKIMessage");
+              "Exception occurred while creating extensions for PKIMessage", ioe);
+      LOG.error("Exception occurred while creating extensions for PKIMessage");
       throw cmpClientException;
     }
     return extGenerator.generate();
@@ -173,7 +196,7 @@ public final class CmpMessageHelper {
         | SignatureException ex) {
       CmpClientException cmpClientException =
           new CmpClientException(
-              "Exception occurred while creating proof " + "of possession for PKIMessage", ex);
+              "Exception occurred while creating proof of possession for PKIMessage", ex);
       LOG.error("Exception occurred while creating proof of possession for PKIMessage");
       throw cmpClientException;
     }
@@ -230,12 +253,260 @@ public final class CmpMessageHelper {
     } catch (NoSuchAlgorithmException | NoSuchProviderException | InvalidKeyException ex) {
       CmpClientException cmpClientException =
           new CmpClientException(
-              "Exception occurred while generating " + "proof of possession for PKIMessage", ex);
+              "Exception occurred while generating proof of possession for PKIMessage", ex);
       LOG.error("Exception occured while generating the proof of possession for PKIMessage");
       throw cmpClientException;
     }
     DERBitString bs = new DERBitString(out);
 
     return new PKIMessage(pkiHeader, pkiBody, bs);
+  }
+
+  public static void checkIfCmpResponseContainsError(PKIMessage respPkiMessage)
+      throws CmpClientException {
+    if (respPkiMessage.getBody().getType() == PKIBody.TYPE_ERROR) {
+      final ErrorMsgContent errorMsgContent =
+          (ErrorMsgContent) respPkiMessage.getBody().getContent();
+      PkiErrorException pkiErrorException =
+          new PkiErrorException(
+              errorMsgContent.getPKIStatusInfo().getStatusString().getStringAt(0).getString());
+      CmpClientException cmpClientException =
+          new CmpClientException("Error in the PkiMessage response", pkiErrorException);
+      LOG.error("Error in the PkiMessage response: {} ", pkiErrorException.getMessage());
+      throw cmpClientException;
+    }
+  }
+
+  /**
+   * @param cert byte array that contains certificate
+   * @param returnType the type of Certificate to be returned, for example X509Certificate.class.
+   *     Certificate.class can be used if certificate type is unknown.
+   * @throws CertificateParsingException if the byte array does not contain a proper certificate.
+   */
+  public static <T extends Certificate> T getCertfromByteArray(byte[] cert, Class<T> returnType)
+      throws CertificateParsingException, CmpClientException {
+    LOG.info("getting certificate from byte array");
+    return getCertfromByteArray(cert, BouncyCastleProvider.PROVIDER_NAME, returnType);
+  }
+
+  /**
+   * @param cert byte array that contains certificate
+   * @param provider provider used to generate certificate from bytes
+   * @param returnType the type of Certificate to be returned, for example X509Certificate.class.
+   *     Certificate.class can be used if certificate type is unknown.
+   * @throws CertificateParsingException if the byte array does not contain a proper certificate.
+   */
+  public static <T extends Certificate> T getCertfromByteArray(
+      byte[] cert, String provider, Class<T> returnType)
+      throws CertificateParsingException, CmpClientException {
+    String prov = provider;
+    if (provider == null) {
+      prov = BouncyCastleProvider.PROVIDER_NAME;
+    }
+
+    if (returnType.equals(X509Certificate.class)) {
+      return returnType.cast(parseX509Certificate(prov, cert));
+    }
+    return null;
+  }
+
+  /**
+   * Check the certificate with CA certificate.
+   *
+   * @param caCertChain Collection of X509Certificates. May not be null, an empty list or a
+   *     Collection with null entries.
+   * @throws CmpClientException if verification failed
+   */
+  public static void verify(List<X509Certificate> caCertChain) throws CmpClientException {
+    int iterator = 1;
+    while (iterator < caCertChain.size()) {
+      verify(caCertChain.get(iterator - 1), caCertChain.get(iterator), null);
+      iterator += 1;
+    }
+  }
+
+  /**
+   * Check the certificate with CA certificate.
+   *
+   * @param certificate X.509 certificate to verify. May not be null.
+   * @param caCertChain Collection of X509Certificates. May not be null, an empty list or a
+   *     Collection with null entries.
+   * @param date Date to verify at, or null to use current time.
+   * @param pkixCertPathCheckers optional PKIXCertPathChecker implementations to use during cert
+   *     path validation
+   * @throws CmpClientException if certificate could not be validated
+   */
+  public static void verify(
+      X509Certificate certificate,
+      X509Certificate caCertChain,
+      Date date,
+      PKIXCertPathChecker... pkixCertPathCheckers)
+      throws CmpClientException {
+    try {
+      LOG.info("verifying certificates in cert chain");
+      ArrayList<X509Certificate> certlist = new ArrayList<>();
+      certlist.add(certificate);
+      CertPath cp =
+          CertificateFactory.getInstance("X.509", BouncyCastleProvider.PROVIDER_NAME)
+              .generateCertPath(certlist);
+      TrustAnchor anchor = new TrustAnchor(caCertChain, null);
+      PKIXParameters params = new PKIXParameters(Collections.singleton(anchor));
+      for (final PKIXCertPathChecker pkixCertPathChecker : pkixCertPathCheckers) {
+        params.addCertPathChecker(pkixCertPathChecker);
+      }
+      params.setRevocationEnabled(false);
+      params.setDate(date);
+      CertPathValidator cpv =
+          CertPathValidator.getInstance("PKIX", BouncyCastleProvider.PROVIDER_NAME);
+      PKIXCertPathValidatorResult result = (PKIXCertPathValidatorResult) cpv.validate(cp, params);
+      if (LOG.isDebugEnabled()) {
+        LOG.debug("Certificate verify result:{} ", result.toString());
+      }
+    } catch (CertPathValidatorException cpve) {
+      CmpClientException cmpClientException =
+          new CmpClientException(
+              "Invalid certificate or certificate not issued by specified CA: ", cpve);
+      LOG.error("Invalid certificate or certificate not issued by specified CA: ", cpve);
+      throw cmpClientException;
+    } catch (CertificateException ce) {
+      CmpClientException cmpClientException =
+          new CmpClientException("Something was wrong with the supplied certificate", ce);
+      LOG.error("Something was wrong with the supplied certificate", ce);
+      throw cmpClientException;
+    } catch (NoSuchProviderException nspe) {
+      CmpClientException cmpClientException =
+          new CmpClientException("BouncyCastle provider not found.", nspe);
+      LOG.error("BouncyCastle provider not found.", nspe);
+      throw cmpClientException;
+    } catch (NoSuchAlgorithmException nsae) {
+      CmpClientException cmpClientException =
+          new CmpClientException("Algorithm PKIX was not found.", nsae);
+      LOG.error("Algorithm PKIX was not found.", nsae);
+      throw cmpClientException;
+    } catch (InvalidAlgorithmParameterException iape) {
+      CmpClientException cmpClientException =
+          new CmpClientException(
+              "Either ca certificate chain was empty,"
+                  + " or the certificate was on an inappropriate type for a PKIX path checker.",
+              iape);
+      LOG.error(
+          "Either ca certificate chain was empty, "
+              + "or the certificate was on an inappropriate type for a PKIX path checker.",
+          iape);
+      throw cmpClientException;
+    }
+  }
+
+  /**
+   * Parse a X509Certificate from an array of bytes
+   *
+   * @param provider a provider name
+   * @param cert a byte array containing an encoded certificate
+   * @return a decoded X509Certificate
+   * @throws CertificateParsingException if the byte array wasn't valid, or contained a certificate
+   *     other than an X509 Certificate.
+   */
+  public static X509Certificate parseX509Certificate(String provider, byte[] cert)
+      throws CertificateParsingException, CmpClientException {
+    LOG.info("parsing X509Certificate");
+    final CertificateFactory cf = getCertificateFactory(provider);
+    X509Certificate result;
+    try {
+      result =
+          (X509Certificate)
+              Objects.requireNonNull(cf).generateCertificate(new ByteArrayInputStream(cert));
+    } catch (CertificateException ce) {
+      throw new CertificateParsingException(
+          "Could not parse byte array as X509Certificate ", ce);
+    }
+    if (result != null) {
+      return result;
+    } else {
+      throw new CertificateParsingException("Could not parse byte array as X509Certificate.");
+    }
+  }
+
+  /**
+   * Returns a CertificateFactory that can be used to create certificates from byte arrays and such.
+   *
+   * @param provider Security provider that should be used to create certificates, default BC is
+   *     null is passed.
+   * @return CertificateFactory for creating certificate
+   */
+  public static CertificateFactory getCertificateFactory(final String provider)
+      throws CmpClientException {
+    LOG.info("getting certificate Factory");
+    final String prov;
+    prov = Objects.requireNonNullElse(provider, BouncyCastleProvider.PROVIDER_NAME);
+    try {
+      return CertificateFactory.getInstance("X.509", prov);
+    } catch (NoSuchProviderException nspe) {
+      CmpClientException cmpClientException = new CmpClientException("NoSuchProvider: ", nspe);
+      LOG.error("NoSuchProvider: ", nspe);
+      throw cmpClientException;
+    } catch (CertificateException ce) {
+      CmpClientException cmpClientException = new CmpClientException("CertificateException: ", ce);
+      LOG.error("CertificateException: ", ce);
+      throw cmpClientException;
+    }
+  }
+
+  /**
+   * puts together certChain and Trust store and verifies the certChain
+   *
+   * @param respPkiMessage PKIMessage that may contain extra certs used for certchain
+   * @param certRepMessage CertRepMessage that should contain rootCA for certchain
+   * @param leafCertificate certificate returned from our original Cert Request
+   * @param certChain Array of certificates to be used for KeyStore
+   * @param trustStore Array of certificates to be used for TrustStore
+   * @return list of two lists, CertChain and TrustStore
+   * @throws CertificateParsingException thrown if error occurs while parsing certificate
+   * @throws IOException thrown if IOException occurs while parsing certificate
+   * @throws CmpClientException thrown if error occurs during the verification of the certChain
+   */
+  public static List<ArrayList<X509Certificate>> verifyAndReturnCertChainAndTrustSTore(
+      PKIMessage respPkiMessage,
+      CertRepMessage certRepMessage,
+      X509Certificate leafCertificate,
+      ArrayList<X509Certificate> certChain,
+      ArrayList<X509Certificate> trustStore)
+      throws CertificateParsingException, IOException, CmpClientException {
+    LOG.info("verifying the certificates in the cert chain.");
+    certChain.add(leafCertificate);
+    addExtraCertsToChain(respPkiMessage, certRepMessage, certChain);
+    CmpMessageHelper.verify(certChain);
+    ArrayList<ArrayList<X509Certificate>> listOfArray = new ArrayList<>();
+    listOfArray.add(certChain);
+    listOfArray.add(trustStore);
+    return listOfArray;
+  }
+
+  /**
+   * checks whether PKIMessage contains extracerts to create certchain, if not creates from caPubs
+   *
+   * @param respPkiMessage PKIMessage that may contain extra certs used for certchain
+   * @param certRepMessage CertRepMessage that should contain rootCA for certchain
+   * @param certChain Array of certificates to be used for KeyStore
+   * @throws CertificateParsingException thrown if error occurs while parsing certificate
+   * @throws IOException thrown if IOException occurs while parsing certificate
+   * @throws CmpClientException thrown if there are errors creating CertificateFactory
+   */
+  public static void addExtraCertsToChain(
+      PKIMessage respPkiMessage,
+      CertRepMessage certRepMessage,
+      ArrayList<X509Certificate> certChain)
+      throws CertificateParsingException, IOException, CmpClientException {
+    LOG.info("adding certificates to chain");
+    if (respPkiMessage.getExtraCerts() != null) {
+      final CMPCertificate[] extraCerts = respPkiMessage.getExtraCerts();
+      for (CMPCertificate cmpCert : extraCerts) {
+        certChain.add(
+            CmpMessageHelper.getCertfromByteArray(cmpCert.getEncoded(), X509Certificate.class));
+      }
+    } else {
+      final CMPCertificate respCmpCaCert = certRepMessage.getCaPubs()[0];
+      certChain.add(
+          CmpMessageHelper.getCertfromByteArray(respCmpCaCert.getEncoded(), X509Certificate.class));
+    }
   }
 }
