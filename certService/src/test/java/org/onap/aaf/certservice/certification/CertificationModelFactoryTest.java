d2b0b5e3b0ff0cf55d6610ecf2657aab7862a59b
Bartosz Gardziejewski <bartosz.gardziejewski@nokia.com>
2020-02-26 09:08:35 +0100

    Create logic responsible for selecting server based on CAName
    Issue-ID: AAF-995
    Signed-off-by: Bartosz Gardziejewski <bartosz.gardziejewski@nokia.com>
    Change-Id: I2d3b931862ecca7c1edc52fb61aa8c4dcd29e3ec

--- certService/src/test/java/org/onap/aaf/certservice/certification/CertificationModelFactoryTest.java
+++ certService/src/test/java/org/onap/aaf/certservice/certification/CertificationModelFactoryTest.java
@@ -22,26 +22,39 @@ package org.onap.aaf.certservice.certification;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.onap.aaf.certservice.certification.configuration.Cmpv2ServerProvider;
+import org.onap.aaf.certservice.certification.configuration.model.Cmpv2Server;
+import org.onap.aaf.certservice.certification.exception.Cmpv2ServerNotFoundException;
 import org.onap.aaf.certservice.certification.model.CertificationModel;
 import org.onap.aaf.certservice.certification.model.CsrModel;
 
+import java.util.Optional;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 import static org.onap.aaf.certservice.certification.CertificationData.CA_CERT;
 import static org.onap.aaf.certservice.certification.CertificationData.ENTITY_CERT;
 import static org.onap.aaf.certservice.certification.CertificationData.INTERMEDIATE_CERT;
 import static org.onap.aaf.certservice.certification.CertificationData.EXTRA_CA_CERT;
-
 
 class CertificationModelFactoryTest {
 
 
     private CertificationModelFactory certificationModelFactory;
 
+    @Mock
+    Cmpv2ServerProvider cmpv2ServerProvider;
+
     @BeforeEach
     void setUp() {
-        certificationModelFactory = new CertificationModelFactory();
+        MockitoAnnotations.initMocks(this);
+        certificationModelFactory = new CertificationModelFactory(cmpv2ServerProvider);
     }
 
     @Test
@@ -49,6 +62,7 @@ class CertificationModelFactoryTest {
         // given
         final String testCaName = "testCA";
         CsrModel mockedCsrModel = mock(CsrModel.class);
+        when(cmpv2ServerProvider.getCmpv2Server(testCaName)).thenReturn(Optional.of(createTestCmpv2Server()));
 
         // when
         CertificationModel certificationModel = certificationModelFactory.createCertificationModel(mockedCsrModel ,testCaName);
@@ -60,4 +74,25 @@ class CertificationModelFactoryTest {
         assertThat(certificationModel.getTrustedCertificates()).contains(CA_CERT, EXTRA_CA_CERT);
     }
 
+    @Test
+    void shouldThrowCmpv2ServerNotFoundExceptionWhenGivenWrongCaName() {
+        // given
+        final String testCaName = "testCA";
+        String expectedMessage = "CA not found";
+        CsrModel mockedCsrModel = mock(CsrModel.class);
+        when(cmpv2ServerProvider.getCmpv2Server(testCaName)).thenThrow(new Cmpv2ServerNotFoundException(expectedMessage));
+
+        // when
+        Exception exception = assertThrows(
+                Cmpv2ServerNotFoundException.class, () ->
+                        certificationModelFactory.createCertificationModel(mockedCsrModel ,testCaName)
+        );
+
+        // then
+        assertTrue(exception.getMessage().contains(expectedMessage));
+    }
+
+    private Cmpv2Server createTestCmpv2Server() {
+        return new Cmpv2Server();
+    }
 }
