8dcc83c37f1f128bb33de7f97b4ce598e273e6b1
Bartosz Gardziejewski <bartosz.gardziejewski@nokia.com>
2020-02-26 09:08:35 +0100

    Create logic responsible for selecting server based on CAName
    Issue-ID: AAF-995
    Signed-off-by: Bartosz Gardziejewski <bartosz.gardziejewski@nokia.com>
    Change-Id: I2d3b931862ecca7c1edc52fb61aa8c4dcd29e3ec

--- certService/src/main/java/org/onap/aaf/certservice/certification/configuration/model/Cmpv2Server.java
+++ certService/src/main/java/org/onap/aaf/certservice/certification/configuration/model/Cmpv2Server.java
@@ -77,4 +77,14 @@ public class Cmpv2Server {
         this.url = url;
     }
 
+    @Override
+    public String toString() {
+        return "Cmpv2Server{" +
+                "authentication=" + authentication +
+                ", caMode=" + caMode +
+                ", caName='" + caName + '\'' +
+                ", issuerDN='" + issuerDN + '\'' +
+                ", url='" + url + '\'' +
+                '}';
+    }
 }
