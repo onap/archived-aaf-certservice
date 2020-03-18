50a1b4d76dc892e614d87f6365ab4da53f3491c8
mharazin <mateusz.harazin@nokia.com>
2020-03-18 10:06:22 +0100

    Improve readability of logging for happy cases
    Issue-ID: AAF-1107
    Signed-off-by: Mateusz Harazin <mateusz.harazin@nokia.com>
    Change-Id: If6501ad59955cac6611bc233d3fcd4ef95829b66

--- certServiceClient/src/main/java/org/onap/aaf/certservice/client/certification/CsrFactory.java
+++ certServiceClient/src/main/java/org/onap/aaf/certservice/client/certification/CsrFactory.java
@@ -40,9 +40,7 @@ import org.slf4j.LoggerFactory;
 import javax.security.auth.x500.X500Principal;
 import java.io.IOException;
 import java.io.StringWriter;
-import java.nio.charset.StandardCharsets;
 import java.security.KeyPair;
-import java.util.Base64;
 import java.util.Optional;
 
 import static org.onap.aaf.certservice.client.certification.EncryptionAlgorithmConstants.COMMON_NAME;
@@ -56,7 +54,7 @@ import static org.onap.aaf.certservice.client.certification.EncryptionAlgorithmC
 
 public class CsrFactory {
 
-    private final Logger LOGGER = LoggerFactory.getLogger(CsrFactory.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(CsrFactory.class);
     private static final String SANS_DELIMITER = ":";
     private final CsrConfiguration configuration;
 
@@ -67,10 +65,13 @@ public class CsrFactory {
 
 
     public String createCsrInPem(KeyPair keyPair) throws CsrGenerationException {
+        LOGGER.info("Creation of Csr has been started with following parameters: {}", configuration.toString());
         PKCS10CertificationRequest request;
         String csrParameters = getMandatoryParameters().append(getOptionalParameters()).toString();
         X500Principal subject = new X500Principal(csrParameters);
         request = createPKCS10Csr(subject, keyPair);
+
+        LOGGER.info("Creation of Csr has been completed successfully");
         return convertPKC10CsrToPem(request);
     }
 
