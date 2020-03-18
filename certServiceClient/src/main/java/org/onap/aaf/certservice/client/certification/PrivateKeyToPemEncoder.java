ebacb12fcfccd2f00856e8f8277856b910f7bfd2
mharazin <mateusz.harazin@nokia.com>
2020-03-18 10:06:22 +0100

    Improve readability of logs for positive path
    Issue-ID: AAF-1107
    Signed-off-by: Mateusz Harazin <mateusz.harazin@nokia.com>
    Change-Id: If6501ad59955cac6611bc233d3fcd4ef95829b66

--- certServiceClient/src/main/java/org/onap/aaf/certservice/client/certification/PrivateKeyToPemEncoder.java
+++ certServiceClient/src/main/java/org/onap/aaf/certservice/client/certification/PrivateKeyToPemEncoder.java
@@ -34,14 +34,15 @@ import org.slf4j.LoggerFactory;
 
 public class PrivateKeyToPemEncoder {
 
-    public static final String PEM_OBJECT_TYPE = "RSA PRIVATE KEY";
-    private final Logger LOGGER = LoggerFactory.getLogger(PrivateKeyToPemEncoder.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(PrivateKeyToPemEncoder.class);
+    private static final String PEM_OBJECT_TYPE = "RSA PRIVATE KEY";
 
     public String encodePrivateKeyToPem(PrivateKey pk) throws PkEncodingException {
         LOGGER.info("Encoding PrivateKey to PEM");
         StringWriter stringWriter = new StringWriter();
         try (JcaPEMWriter pemWriter = new JcaPEMWriter(stringWriter)) {
             pemWriter.writeObject(new PemObject(PEM_OBJECT_TYPE, pk.getEncoded()));
+            LOGGER.info("PrivateKey to PEM encoding has been started");
         } catch (IOException e) {
             LOGGER.error("Exception occurred during encoding PrivateKey to PEM", e);
             throw new PkEncodingException(e);
