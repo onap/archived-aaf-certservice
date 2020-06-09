2194da15dd26e676f0b9235d4430b1b8b3db6636
Tomasz Wrobel <tomasz.wrobel@nokia.com>
2020-06-09 15:37:46 +0200

    Refactor flow of cert files generation, based on OUTPUT_TYPE parameter
    -Add strategy pattern
    -Refactor KeystoreTruststoreCreator
    -Change Unit tests
    
    Issue-ID: AAF-1152
    Signed-off-by: Tomasz Wrobel <tomasz.wrobel@nokia.com>
    Change-Id: If2b2fa50d551e72f19319d781bfb6079d07c7b83

--- certServiceClient/src/test/java/org/onap/aaf/certservice/client/certification/conversion/PKCS12ArtifactCreatorTest.java
+++ certServiceClient/src/test/java/org/onap/aaf/certservice/client/certification/conversion/PKCS12ArtifactCreatorTest.java
@@ -26,10 +26,11 @@ import static org.mockito.Mockito.when;
 
 import java.security.PrivateKey;
 import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.onap.aaf.certservice.client.certification.exception.PemToPKCS12ConverterException;
 
-class KeystoreTruststoreCreatorTest {
+class PKCS12ArtifactCreatorTest {
 
     private PKCS12FilesCreator filesCreator = mock(PKCS12FilesCreator.class);
     private RandomPasswordGenerator passwordGenerator = mock(RandomPasswordGenerator.class);
@@ -44,7 +45,7 @@ class KeystoreTruststoreCreatorTest {
         final int passwordLength = 24;
         final String alias = "certificate";
         final byte[] keystoreBytes = "this is a keystore test".getBytes();
-        KeystoreTruststoreCreator creator = new KeystoreTruststoreCreator(filesCreator, passwordGenerator, converter);
+        PKCS12ArtifactCreator creator = new PKCS12ArtifactCreator(filesCreator, passwordGenerator, converter);
 
         // when
         when(passwordGenerator.generate(passwordLength)).thenReturn(password);
@@ -61,20 +62,57 @@ class KeystoreTruststoreCreatorTest {
     void createTruststoreShouldCallRequiredMethods() throws PemToPKCS12ConverterException {
         // given
         final Password password = new Password("d9D_u8LooYaXH4G48DtN#vw0");
-        final List<String> certificates = List.of("a", "b");
+        final List<String> certificateChain = List.of("a", "b");
+        final List<String> trustAnchors = List.of("a", "b");
         final int passwordLength = 24;
         final String alias = "trusted-certificate-";
         final byte[] truststoreBytes = "this is a truststore test".getBytes();
-        KeystoreTruststoreCreator creator = new KeystoreTruststoreCreator(filesCreator, passwordGenerator, converter);
+        PKCS12ArtifactCreator creator = new PKCS12ArtifactCreator(filesCreator, passwordGenerator, converter);
 
         // when
         when(passwordGenerator.generate(passwordLength)).thenReturn(password);
-        when(converter.convertTruststore(certificates, password, alias)).thenReturn(truststoreBytes);
-        creator.createTruststore(certificates);
+        when(converter.convertTruststore(certificateChain, password, alias)).thenReturn(truststoreBytes);
+        creator.createTruststore(certificateChain);
 
         // then
         verify(passwordGenerator, times(1)).generate(passwordLength);
-        verify(converter, times(1)).convertTruststore(certificates, password, alias);
+        verify(converter, times(1)).convertTruststore(trustAnchors, password, alias);
+        verify(filesCreator, times(1)).saveTruststoreData(truststoreBytes, password.getCurrentPassword());
+    }
+    //TODO
+    // 1. Password generator should be called twice times
+    // 2. Convert keystore should be called
+    // 3. Keystore file creator should be called -> KeystoreCreationFuncion Should Be Called
+    // 2. Convert truststore func should be called
+    // 3. Trustoster file creator should be called -> TrustStore creation function should be called
+    @Test
+    void generatePKCS12ArtifactShouldCallRequiredMethods() throws PemToPKCS12ConverterException {
+        // given
+        final Password password = new Password("d9D_u8LooYaXH4G48DtN#vw0");
+        final List<String> certificateChain = List.of("a", "b");
+        final List<String> trustAnchors = List.of("a", "b");
+        final int passwordLength = 24;
+        final String certificateAlias = "certificate";
+        final String trustedCertificateAlias = "trusted-certificate-";
+        final byte[] keystoreBytes = "this is a keystore test".getBytes();
+        final byte[] truststoreBytes = "this is a truststore test".getBytes();
+        PKCS12ArtifactCreator creator = new PKCS12ArtifactCreator(filesCreator, passwordGenerator, converter);
+
+        // when
+        when(passwordGenerator.generate(passwordLength)).thenReturn(password);
+        when(converter.convertKeystore(certificateChain, password, certificateAlias, privateKey)).thenReturn(keystoreBytes);
+        when(converter.convertTruststore(trustAnchors, password, trustedCertificateAlias)).thenReturn(truststoreBytes);
+//        creator.createKeystore(certificateChain, privateKey);
+        creator.generateArtifacts(certificateChain, trustAnchors, privateKey);
+
+        //then
+        // then
+        verify(passwordGenerator, times(2)).generate(passwordLength);
+        verify(converter, times(1)).convertKeystore(
+                certificateChain, password, certificateAlias, privateKey);
+        verify(filesCreator, times(1)).saveKeystoreData(
+                keystoreBytes, password.getCurrentPassword());
+        verify(converter, times(1)).convertTruststore(trustAnchors, password, trustedCertificateAlias);
         verify(filesCreator, times(1)).saveTruststoreData(truststoreBytes, password.getCurrentPassword());
     }
 }
