858e4679ab66e385a123917e9d0ef5c8d9bb0b92
pwielebs <piotr.wielebski@nokia.com>
2020-06-10 11:02:19 +0200

    Add validation for OUTPUT_TYPE parameter
    Issue-ID: AAF-1152
    
    Signed-off-by: pwielebs <piotr.wielebski@nokia.com>
    Change-Id: Iec46b8a7b8e1818b62877f5272368636dc1929a0

--- certServiceClient/src/main/java/org/onap/aaf/certservice/client/configuration/factory/AbstractConfigurationFactory.java
+++ certServiceClient/src/main/java/org/onap/aaf/certservice/client/configuration/factory/AbstractConfigurationFactory.java
@@ -20,6 +20,8 @@
 
 package org.onap.aaf.certservice.client.configuration.factory;
 
+
+import org.onap.aaf.certservice.client.configuration.OutputTypes;
 import org.onap.aaf.certservice.client.configuration.exception.ClientConfigurationException;
 import org.onap.aaf.certservice.client.configuration.exception.CsrConfigurationException;
 import org.onap.aaf.certservice.client.configuration.model.ConfigurationModel;
@@ -53,6 +55,12 @@ public abstract class AbstractConfigurationFactory<T extends ConfigurationModel>
 
     public boolean isCountryValid(String country) {
         return Arrays.asList(Locale.getISOCountries()).contains(country);
+    }
+
+    public boolean isOutputTypeValid(String outputType) {
+        return OutputTypes.PEM.getOutputType().equals(outputType) ||
+                OutputTypes.JKS.getOutputType().equals(outputType) ||
+                OutputTypes.P12.getOutputType().equals(outputType);
     }
 
     private boolean isPortNumberPresent(String stringToCheck) {
