4269a92fe0dc29b5dfbcfcd19ed6532cb9b1d6e7
Joanna Jeremicz <joanna.jeremicz@nokia.com>
2020-06-17 10:48:20 +0200

    Add PEM artifacts creation with unit tests
    Issue-ID: AAF-1152
    Change-Id: I95afd62330f3111f916507d628d142262ff951cc
    Signed-off-by: Joanna Jeremicz <joanna.jeremicz@nokia.com>

--- certServiceClient/src/main/java/org/onap/aaf/certservice/client/certification/conversion/ArtifactsCreatorProvider.java
+++ certServiceClient/src/main/java/org/onap/aaf/certservice/client/certification/conversion/ArtifactsCreatorProvider.java
@@ -18,27 +18,32 @@
  */
 package org.onap.aaf.certservice.client.certification.conversion;
 
+import org.onap.aaf.certservice.client.certification.PrivateKeyToPemEncoder;
+import org.onap.aaf.certservice.client.certification.writer.CertFileWriter;
+
 public enum ArtifactsCreatorProvider {
 
     P12 {
         @Override
-        ArtifactsCreator create(String outputPath) {
+        ArtifactsCreator create(String destPath) {
             return new PKCS12ArtifactsCreator(
-                    new PKCS12FilesCreator(outputPath),
+                    new CertFileWriter(destPath),
                     new RandomPasswordGenerator(),
                     new PemToPKCS12Converter());
         }
     },
     JKS {
         @Override
-        ArtifactsCreator create(String outputPath) {
+        ArtifactsCreator create(String destPath) {
             return null;
         }
     },
     PEM {
         @Override
-        ArtifactsCreator create(String outputPath) {
-            return null;
+        ArtifactsCreator create(String destPath) {
+            return new PemArtifactsCreator(
+                    new CertFileWriter(destPath),
+                    new PrivateKeyToPemEncoder());
         }
     };
 
